54659454c424faa509832d30517f4391b0575bdd
Sifat Ullah <97360334+webtragic@users.noreply.github.com>
2022-09-05 10:02:40 +0500

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-fst_num = input('Enter your first whole number= ')
-sec_num = input('Enter your second  number= ')
-sum = int(fst_num) + float(sec_num)
-print('sum=' , sum)
-mul =   int(fst_num) * float(sec_num)
-print('multiplicaiton= ' ,  mul)
