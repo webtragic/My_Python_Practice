cecd88c0625b16b9bba31c7d7dda14c8873886d5
Sifat Ullah <97360334+webtragic@users.noreply.github.com>
2022-09-05 11:10:54 +0500

    Add files via upload

---
+++ Basics of python 1.py
@@ -0,0 +1,15 @@
+print("this is my first practice in python")
+age = 20
+price = 15.6
+first_name = 'webtragic'
+is_online = False
+print(age)
+print("ipnut practice")
+name = input("what is your name? ")
+print("hello " + name)
+print("data types in python")
+birth_year = input("enter your birth year")
+age = 2022 - int(birth_year)
+print(age)
+
+
