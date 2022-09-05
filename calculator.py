59b8b630de6d12106fdf3ff9cc9dff701b396a7e
Sifat Ullah <97360334+webtragic@users.noreply.github.com>
2022-09-05 10:03:08 +0500

    Delete calculator.py

--- calculator.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-req = requests.get("https://etea.edu.pk/result")
-soup = BeautifulSoup(req.content, "html.parser")
-
-res = soup.get(id="899")
-
-print(soup.res)
