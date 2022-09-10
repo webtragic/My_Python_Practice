9b8583fcabd01c79863577f826bb1a4d07d77a7a
Sifat Ullah <97360334+webtragic@users.noreply.github.com>
2022-09-10 12:55:54 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+fst_num = input('Enter your first whole number= ')
+sec_num = input('Enter your second  number= ')
+sum = int(fst_num) + float(sec_num)
+print('sum=' , sum)
+mul =   int(fst_num) * float(sec_num)
+print('multiplicaiton= ' ,  mul)
+sub =   int(fst_num) - float(sec_num)
+print('subtration= ' ,  sub)
+div =   int(fst_num) / float(sec_num)
+print('division= ' ,  div)
+
+
